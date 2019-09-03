038bddb3e50fa8034aede454945bd4b17498cf17
David J Pugh <djpugh@gmail.com>
2019-09-03 17:32:24 +0100

    Auth refactor finished

--- src/docserver/core.py
+++ src/docserver/core.py
@@ -9,6 +9,7 @@ from docserver import __version__
 from docserver.api import api_version
 from docserver.api.base import router as base_api
 from docserver.api.base import router as docs_api
+from docserver.auth.routes import routes as auth_routes
 from docserver.config import config
 from docserver.permissions.staticfiles import PermissionedStaticFiles, DBPermissionsCheck
 from docserver.ui.help import build_help
@@ -27,7 +28,7 @@ app = FastAPI(title='Documentation Server',
               openapi_url=f"/api/v{api_version}/openapi.json",
               docs_url='/api/docs',
               redoc_url='/api/redoc',
-              routes=index_routes+search_routes)
+              routes=index_routes+search_routes+auth_routes)
 
 config.auth.set_middleware(app)
 
